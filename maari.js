940f299fa7822dc466053d1c0dbfe769eac2e918
Bharathvaj <bharathvaj1995@gmail.com>
2017-10-09 02:10:10 +0530

    Fix the eslint configuration issue

--- maari.js
+++ maari.js
@@ -1,41 +1,42 @@
 'use strict';
 
-function test(callback) {
-	const fakeAdFiles = ['ads.js', 'ad.js', 'showAds.js', 'ads.min.js'];
-	let adBlockPresent = false;
+(window => {
+	const maari = callback => {
+		const fakeAdFiles = ['ads.js', 'ad.js', 'showAds.js', 'ads.min.js'];
 
-	const ScriptLoader = function() {
-		const promises = [];
+		const ScriptLoader = function() {
+			const promises = [];
 
-		const _add = _url => {
-			const promise = new Promise((resolve, reject) => {
-				const $script = document.createElement('script');
-				$script.src = _url;
+			const _add = _url => {
+				const promise = new Promise((resolve, reject) => {
+					const $script = document.createElement('script');
+					$script.src = _url;
 
-				$script.addEventListener('load', () => {
-					resolve($script);
+					$script.addEventListener('load', () => {
+						resolve($script);
+					});
+
+					$script.addEventListener('error', () => {
+						reject($script);
+					});
 				});
-
-				$script.addEventListener('error', () => {
-					resolve($script);
-				});
-			});
-			promises.push(promise);
+				promises.push(promise);
+			};
+			const _loaded = cb => {
+				Promise.all(promises).then(null, cb());
+			};
+			return {
+				add: _add,
+				loaded: _loaded
+			};
 		};
-		const _loaded = cb => {
-			Promise.all(promises).then(null, cb());
-		};
-		return {
-			add: _add,
-			loaded: _loaded
-		};
+		const scriptLoader = new ScriptLoader();
+		fakeAdFiles.forEach(fakeFile => {
+			scriptLoader.add(fakeFile);
+		});
+		scriptLoader.loaded(() => {
+			callback();
+		});
 	};
-	const scriptLoader = new ScriptLoader();
-	fakeAdFiles.forEach(fakeFile => {
-		scriptLoader.add(fakeFile);
-	});
-	scriptLoader.loaded(() => {
-		adBlockPresent = true;
-		callback();
-	});
-}
+	window.maari = maari;
+})(window);
